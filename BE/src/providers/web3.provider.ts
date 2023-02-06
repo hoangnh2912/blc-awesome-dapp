1c8daf1f5efd909f01b25e82c85de1bf6e82672e
hoangnh2 <hoangnh2@vmodev.com>
2023-02-06 18:14:15 +0700

    fix env var

--- BE/src/providers/web3.provider.ts
+++ BE/src/providers/web3.provider.ts
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(`${process.env.WEB3_PROVIDER_URI}`);
+const web3 = new Web3(`${process.env.NETWORK_RPC}`);
 
 const newContract = (abi: any, address: string): any => {
   return new web3.eth.Contract(abi, address);
